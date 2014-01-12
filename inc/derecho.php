0edc6b2f0949abd437baed7b9657f7c27adf447b
rafa <rafa@atlanta>
2014-01-12 23:43:11 +0100

    Como evitar la caché de la imágen del perfil.

--- inc/derecho.php
+++ inc/derecho.php
@@ -7,7 +7,12 @@
 		{
 			echo "<h2>Your profile picture</h2>";
 			echo '<ul class="small-image-list">';
-			echo '<li><img src="../fotos/'.$nombrefinal.'" alt="'.$_SESSION['nickname'].'?'.time().'" class="left" />';
+			echo '<li><img src="../fotos/'.$nombrefinal.'?t='.time().'" alt="'.$_SESSION['nickname'].'" class="left" />';
+			// El parametro t=time() se utiliza para que cada vez
+			// que se actualice la página recargue siempre la
+			// fotografía del servidor.
+			// Ejemplo: img src=fotografia.jpg?t=234234234
+			//          img src=fotografia.jpg?t=234234256
 			echo '<h4>'.$_SESSION['personalinfo'].'</h4>';
 			echo '<p>IP address: '.$_SERVER['REMOTE_ADDR'];
 			echo '<br/>Registration date: '.$_SESSION['datecreated'].'</p>';
